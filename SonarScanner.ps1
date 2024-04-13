30567e643f1a9fe020531b4c675b2e0480efd41b
vankenobi <musakucuk99@gmail.com>
2024-04-13 17:37:36 +0300

    rm cached applied

--- SonarScanner.ps1
+++
@@ -1,7 +0,0 @@
-dotnet sonarscanner begin /k:"ReferenceApi" /d:sonar.login="sqp_6ae70378a9e5ee382a658c5cd89e0ad86ae2f8bd" /d:sonar.cs.opencover.reportsPaths="./Reference.Api.Test/coverage.xml"
-
-dotnet build --no-incremental
-
-coverlet ./Reference.Api.Test/bin/Debug/net8.0/Reference.Api.Test.dll  --target "dotnet" --targetargs "test --no-build" --format opencover --output "./Reference.Api.Test/coverage.xml"
-
-dotnet sonarscanner end /d:sonar.login="sqp_6ae70378a9e5ee382a658c5cd89e0ad86ae2f8bd"
